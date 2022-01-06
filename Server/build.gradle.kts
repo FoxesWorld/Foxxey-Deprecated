cb185cfef50d52f82b057f456e398a7713592642
vie10 <hrekhviktor@gmail.com>
2022-01-06 09:07:17 +0200

    [Server] Set up dependencies

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -1,3 +1,13 @@
+val ktor = "1.6.7"
+val kotlinCoroutines = "1.6.0"
+val junitBom = "5.8.2"
+val mockito = "4.2.0"
+val hoplite = "1.4.16"
+val kodein = "7.10.0"
+val ktorm = "3.4.1"
+val log4j = "2.17.1"
+val logback = "1.2.9"
+
 plugins {
     kotlin("jvm") version "1.6.10"
 }
@@ -10,5 +20,50 @@ repositories {
 }
 
 dependencies {
+    // Kotlin
     implementation(kotlin("stdlib"))
+    implementation(kotlinCoroutines("core"))
+
+    // Ktor
+    implementation(ktor("server-core"))
+    implementation(ktor("server-netty"))
+    implementation(ktor("network"))
+    implementation(ktor("network-tls"))
+
+    // Configurations
+    implementation(hoplite("core"))
+    implementation(hoplite("json"))
+
+    // Database communication
+    implementation(ktorm("core"))
+
+    // Dependency injection
+    implementation("org.kodein.di", "kodein-di", kodein)
+
+    // Logging
+    implementation(log4j("core"))
+    implementation(log4j("api"))
+    implementation("ch.qos.logback", "logback-classic", logback)
+
+    // Testing
+    testImplementation(kotlinCoroutines("test"))
+    testImplementation(platform("org.junit:junit-bom:$junitBom"))
+    testImplementation("org.junit.jupiter", "junit-jupiter")
+    testImplementation("org.mockito", "mockito-core", mockito)
 }
+
+/*
+
+    The code is only for beautify previous.
+
+ */
+
+fun ktor(name: String) = "io.ktor:ktor-$name:$ktor"
+
+fun kotlinCoroutines(part: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$part:$kotlinCoroutines"
+
+fun hoplite(part: String) = "com.sksamuel.hoplite:hoplite-$part:hoplite"
+
+fun ktorm(part: String) = "org.ktorm:ktorm-$part:$ktorm"
+
+fun log4j(part: String) = "org.apache.logging.log4j:log4j-$part:$log4j"

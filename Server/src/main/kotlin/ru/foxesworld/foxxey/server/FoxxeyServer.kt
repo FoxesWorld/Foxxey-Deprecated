9a63197d2a00c81e51494a1dd114851c3649a32f
vie10 <hrekhviktor@gmail.com>
2022-01-08 06:50:56 +0200

    [Server] Add server config to FoxxeyServer.kt params

--- Server/src/main/kotlin/ru/foxesworld/foxxey/server/FoxxeyServer.kt
+++ Server/src/main/kotlin/ru/foxesworld/foxxey/server/FoxxeyServer.kt
@@ -1,9 +1,16 @@
 package ru.foxesworld.foxxey.server
 
 import ru.foxesworld.foxxey.server.config.InfoConfig
+import ru.foxesworld.foxxey.server.config.ServerConfig
 
+/**
+ * Foxxey [Server] implementation
+ * @param infoConfig The server information config
+ * @param serverConfig The server configuration
+ */
 class FoxxeyServer(
-    infoConfig: InfoConfig
+    infoConfig: InfoConfig,
+    private val serverConfig: ServerConfig
 ) : Server {
 
     override val version: String = infoConfig.version

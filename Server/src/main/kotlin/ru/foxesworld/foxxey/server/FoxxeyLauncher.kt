4be2210ddfde4736b0fc3461dcf9594322d56a3b
vie10 <hrekhviktor@gmail.com>
2022-01-24 23:31:13 +0200

    [Server] Print banner at launch

--- Server/src/main/kotlin/ru/foxesworld/foxxey/server/FoxxeyLauncher.kt
+++ Server/src/main/kotlin/ru/foxesworld/foxxey/server/FoxxeyLauncher.kt
@@ -1,8 +1,12 @@
 package ru.foxesworld.foxxey.server
 
 import kotlinx.coroutines.DelicateCoroutinesApi
+import mu.KotlinLogging
 import org.koin.core.component.inject
 import ru.foxesworld.foxxey.server.commands.CommandHandler
+import ru.foxesworld.foxxey.server.os.OSHelper
+
+private val log = KotlinLogging.logger { }
 
 @DelicateCoroutinesApi
 class FoxxeyLauncher : Launcher {
@@ -10,6 +14,23 @@ class FoxxeyLauncher : Launcher {
     private val commandHandler: CommandHandler by inject()
 
     override fun launch() {
+        printBanner()
         commandHandler.start()
+    }
+
+    private fun printBanner() {
+        log.info {
+            """
+                    |
+                    |┌───────────────────────────
+                    |│ FoxxeyLauncher
+                    |│
+                    |│ You're trying to launch Foxxey at ${OSHelper.machineName}
+                    |│ Operation system: ${OSHelper.osName}
+                    |│ Architecture: ${OSHelper.osArchitecture}
+                    |│ Memory: ${OSHelper.usedMemoryInMB} / ${OSHelper.availableMemoryInMB} megabytes
+                    |└───────────────────────────
+                """.trimMargin()
+        }
     }
 }

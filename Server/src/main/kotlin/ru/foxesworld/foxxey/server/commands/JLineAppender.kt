8ef340ae068e4b8cd9461f3cba80ac6c5fdad8ca
vie10 <hrekhviktor@gmail.com>
2022-01-24 23:29:38 +0200

    [Server] Add ANSI colors support to logging

--- Server/src/main/kotlin/ru/foxesworld/foxxey/server/commands/JLineAppender.kt
+++ Server/src/main/kotlin/ru/foxesworld/foxxey/server/commands/JLineAppender.kt
@@ -4,6 +4,7 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.AppenderBase
 import org.jline.reader.LineReader
+import picocli.CommandLine
 
 @Suppress("MemberVisibilityCanBePrivate")
 class JLineAppender : AppenderBase<ILoggingEvent>() {
@@ -11,7 +12,9 @@ class JLineAppender : AppenderBase<ILoggingEvent>() {
     var lineReader: LineReader? = null
 
     override fun append(eventObject: ILoggingEvent?) {
-        val encoded = String(encoder.encode(eventObject))
+        val encoded = CommandLine.Help.Ansi.AUTO.string(
+            String(encoder.encode(eventObject))
+        )
         lineReader?.printAbove(encoded) ?: println(encoded)
     }
 }
